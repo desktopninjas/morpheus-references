bfa91ffcfc35ce70c02087e5a15b5cbff231a01c
Michael Lucas <michael@desktopninjas.com>
2023-06-27 10:13:02 -0500

    Update provider.tf

--- tf-modules/ecs-standard/provider.tf
+++ tf-modules/ecs-standard/provider.tf
@@ -1,8 +1,28 @@
-variable "region" {
-  description = "The region in which to create the resources."
-  default     = "us-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.4.0"
+    }
+  }
 }
 
 provider "aws" {
+  profile = "hmcp-aws"
   region = var.region
+  access_key = var.ngdc_terraform_access_key
+  secret_key = var.ngdc_terraform_secret_access_key
+}
+
+#Provider 
+variable "region" {
+  type = string
+}
+
+variable "ngdc_terraform_access_key" {
+  type = string
+}
+
+variable "ngdc_terraform_secret_access_key" {
+  type = string
 }
